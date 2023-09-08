fbbdbfd860ec74d4e9b7cce3cc6b0c9fece2ead0
Parvez-Sharief95 <119729990+Parvez-Sharief95@users.noreply.github.com>
2023-09-08 19:49:45 +0530

    import java.util.Scanner; public class Octal {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int c = sc.nextInt();         for(int i=1;i<=c;i++)         {             String s = sc.next();             int m=Integer.parseInt(s,8);             String d = Integer.toString(m,2);             System.out.println(d);         }     } }

---
+++ Octal_to_Binary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Octal
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        for(int i=1;i<=c;i++)
+        {
+            String s = sc.next();
+            int m=Integer.parseInt(s,8);
+            String d = Integer.toString(m,2);
+            System.out.println(d);
+        }
+    }
+}
